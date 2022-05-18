8fe82e826be1818c7b7624f8217c51ee10f5e979
Gizem <gizem.candemir@secilstore.com>
2022-05-18 14:25:08 +0300

    Building the SidebarRow Component

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import {BellIcon, HashtagIcon, BookmarkIcon, CollectionIcon, DotsCircleHorizontalIcon, MailIcon, UserIcon, HomeIcon} from "@heroicons/react/outline"
+import SideBarRow from './SideBarRow'
 
 
 function Sidebar() {
   return (
-    <div>
+    <div className='flex flex-col'>
         <img className='h-10 w-10' src="htttps://links.papareact.com/" alt="" />
-         <BellIcon width={24} height={24} /></div>
+        <SideBarRow Icon={HomeIcon} title="Home" />
+        <SideBarRow Icon={HashtagIcon} title="Explore" />
+        <SideBarRow Icon={BellIcon} title="Notifications" />
+        <SideBarRow Icon={MailIcon} title="Messages" />
+        <SideBarRow Icon={BookmarkIcon} title="Bookmarks" />
+        <SideBarRow Icon={CollectionIcon} title="Lists" />
+        <SideBarRow Icon={UserIcon} title="Sign In" />
+        <SideBarRow Icon={DotsCircleHorizontalIcon} title="More" />
+
+     </div>
   )
 }
 
